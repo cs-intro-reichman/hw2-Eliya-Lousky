a478e162cd2d4a5e0482e9ac943b85610feaacc4
eliya lousky <eliya.lousky@post.runi.ac.il>
2025-11-10 20:39:09 +0200

    1-4

--- Cheers.java
+++ Cheers.java
@@ -9,5 +9,27 @@ Great work boaz!
 public class Cheers {
         public static void main(String[] args) {
 	    // Replace this comment with your code
+            String cheers = (args[0]);
+            int num =Integer.parseInt(args [1]);
+            String Special = "AEFHILMNOR";
+
+        
+            for (int i = 0 ; i <= cheers.length() ; i++){
+                char letter = cheers.charAt(i);
+                if (Special.indexOf(letter) != -1 ) {
+                System.out.println("Give mw an " + letter + ": " + letter + "!");
+                }  else { 
+                System.out.println("Give mw a " + letter + ": " + letter + "!");
+                }
+                }
+
+                System.out.println(" What does that spell?");
+
+                for (int v = 0 ; v <= num ; v++ ){
+                  System.out.println(cheers + "!!!");
+                }
+
+            
+            
         }
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,33 @@
 public class TimeCalc {
     public static void main(String[] args) {
         // Replace this comment with your code
+        String hhmm = (args[0]);
+        String adding = (args[1]);
+
+        String[] parts = hhmm.split(":");
+
+        int mm = Integer.parseInt(parts[0]);
+        int hh = Integer.parseInt(parts[1]);
+
+
+        String minutes = ((hh * 60 ) + adding + mm );
+        int min = Integer.parseInt (minutes);
+        int hours = (min / 60 );
+        int newhours = (hh % 24);
+        int newminutes = (min - (hours*60));
+
+        String hourstr ;
+         if (newhours < 10) {
+         hourstr = "0" + newhours;
+        } else {
+         hourstr = "" + newhours;
+        }
+
+        System.out.println(hhmm + " " + adding);
+
+        System.out.println(hourstr + ":" + newminutes);
+
     }
 }
+
+
